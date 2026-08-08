cea7d1cfd4d9d814fbff9935732051c54ae3ea2f
otsho-pr0 <otsho-pr0@proton.me>
2026-08-08 13:34:52 +0100

    Adding which-key

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -426,4 +426,18 @@ return
 			}
 		}
 	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+		opts = {}
+	}
 }
